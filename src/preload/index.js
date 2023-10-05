0a8b28da2b7e6772d65288c9402ac13c46b9fd04
icnawi <icnawi@outlook.com>
2023-10-04 17:27:51 -0700

    basic app configuration

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,20 +1,23 @@
-import { contextBridge } from 'electron'
-import { electronAPI } from '@electron-toolkit/preload'
-
-// Custom APIs for renderer
-const api = {}
+import { contextBridge } from 'electron';
+import { electronAPI } from '@electron-toolkit/preload';
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
+const { ipcRenderer } = electronAPI;
+
+const darkModeApi = {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system'),
+};
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('electron', electronAPI)
-    contextBridge.exposeInMainWorld('api', api)
+    contextBridge.exposeInMainWorld('electron', electronAPI);
+    contextBridge.exposeInMainWorld('darkMode', darkModeApi);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 } else {
-  window.electron = electronAPI
-  window.api = api
+  window.electron = electronAPI;
+  window.darkMode = darkModeApi;
 }
